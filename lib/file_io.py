39607d209f2de7badabe397f0746ed593d930316
Nyakio-Eva <evalkariuki@gmail.com>
2024-03-11 20:17:07 +0300

    completed assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,24 @@
+
+
 def write_file(file_name, file_content):
-    pass
+    #adding txt extension
+    text_file = str(file_name) + '.txt'
+
+    with open(text_file, mode='w', encoding='utf-8')  as file:
+
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    text_file = str(file_name) + '.txt'
+
+    with open(text_file, mode='a', encoding='utf-8') as file:
+        file.write(append_content)
+
+
 
 def read_file(file_name):
-    pass
+    text_file = str(file_name) + '.txt'
+    with open(text_file, encoding='utf-8') as file:
+        file_content = file.read()
+        return file_content
+
